df7217872314ac321eb51a9ae1ff8167c02e7778
Stefania Costache <stefania.costache@gmail.com>
2007-09-16 20:59:47 +0300

    forgot the cmcallbacks.c file

---
+++ src/cmcallbacks.c
@@ -0,0 +1,57 @@
+/**
+* Cache Manager Callbacks
+**/
+
+#include <lklvfs.h>
+
+//
+//	acquire for lazy write
+//
+BOOLEAN DDKAPI VfsAcqLazyWrite(PVOID context, BOOLEAN wait)
+{
+	PLKLFCB	fcb = (PLKLFCB) context;
+
+	ASSERT(fcb);
+	ASSERT((fcb->id.type == FCB) && (fcb->id.size == sizeof(LKLFCB)));
+
+	return ExAcquireResourceExclusiveLite(&fcb->fcb_resource, wait);
+}
+
+//
+//	release from lazy write
+//
+void DDKAPI VfsRelLazyWrite(PVOID context)
+{
+	PLKLFCB	fcb = (PLKLFCB)context;
+
+	ASSERT(fcb);
+	ASSERT((fcb->id.type == FCB) && (fcb->id.size == sizeof(PLKLFCB)));
+
+	RELEASE(&fcb->fcb_resource);
+}
+
+//
+//	acquire for read ahead
+//
+BOOLEAN DDKAPI VfsAcqReadAhead(PVOID context, BOOLEAN wait)
+{
+	PLKLFCB fcb = (PLKLFCB) context;
+
+	ASSERT(fcb);
+	ASSERT((fcb->id.type == FCB) && (fcb->id.size == sizeof(PLKLFCB)));
+	
+	return ExAcquireResourceSharedLite(&(fcb->fcb_resource), wait);
+}
+
+//
+//	release from read ahead
+//
+void DDKAPI VfsRelReadAhead(PVOID context)
+{
+	PLKLFCB fcb = (PLKLFCB) context;
+
+	ASSERT(fcb);
+	ASSERT((fcb->id.type == FCB) && (fcb->id.size == sizeof(PLKLFCB)));
+
+	RELEASE(&(fcb->fcb_resource));
+}
